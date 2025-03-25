2d55fd3669f5d923d8bd3826f69721626a2ce98e
girgir <wjdrlf5986@naver.com>
2025-03-25 22:31:19 +0900

    프로젝트 초기 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
+const projectName = 'vending-machine';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@images': resolve(__dirname, 'public', 'resources', 'images'),
+    },
+  },
+  base: '',
+  build: {
+    cssMinify: false,
+    minify: false,
+    outDir: './build',
+
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        inlineDynamicImports: false,
+        assetFileNames: (assetInfo) => {
+          // Ensure assetInfo.name is defined
+          if (!assetInfo.name) {
+            return `resources/other/[name][extname]`; // Default fallback
+          }
+
+          let extType = assetInfo.name.match(/\.([a-z0-9]+)$/i)?.[1];
+          if (/(css)/i.test(extType || '')) {
+            return `resources/${extType}/${projectName}-[hash][extname]`;
+          } else if (/(png|jpe?g|svg|gif|tiff|bmp|ico)/i.test(extType || '')) {
+            extType = 'images';
+          } else if (/(woff2|woff|otf|ttf)/i.test(extType || '')) {
+            extType = 'fonts';
+          }
+          return `resources/${extType || 'other'}/[name][extname]`; // Default fallback
+        },
+        chunkFileNames: `resources/js/${projectName}-[hash].js`,
+        entryFileNames: `resources/js/${projectName}-[hash].js`,
+      },
+    },
+  },
+
   plugins: [react()],
-})
+});
